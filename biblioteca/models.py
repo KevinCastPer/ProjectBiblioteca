3fa177bcc0dc4c9c8087bd22eef7df4572121821
KevinCastPer <kecasper@gmail.com>
2018-12-03 18:42:56 +0100

    añadir orden de nombre y crear funcion __str__ a todos modelos

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -11,13 +11,26 @@ class Editor(models.Model):
     pais = models.CharField(max_length=50)
     website = models.URLField()
 
+    class Meta:
+        ordering = ["nombre"]
+
+    def __str__(self):
+        return self.nombre
+
+
 class Autor(models.Model):
     nombre = models.CharField(max_length=30)
     apellidos = models.CharField(max_length=60)
     email = models.EmailField()
+
+    def __str__(self):
+        return "%s %s"%(self.nombre, self.apellidos)
 
 class Libro(models.Model):
     titulo = models.CharField(max_length=100)
     autores = models.ManyToManyField(Autor)
     fecha_publicacion = models.DateField()
     portada = models.ImageField(upload_to='portadas')
+
+    def __str__(self):
+        return self.titulo
